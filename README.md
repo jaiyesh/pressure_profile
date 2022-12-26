299260718c988ae4f8677e3cca26216abc7700e2
jaiyesh <jaiyesh0002@gmail.com>
2022-12-26 21:43:38 +0530

    updated new

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pressure_profile
 Pressure Profile App using Streamlit
+
+An UI app for visualizing pressure profile
--- streamlit.py
+++ streamlit.py
@@ -1,37 +1,34 @@
 import streamlit as st
 
-
+from PIL import Image
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+image = Image.open('logo.jpg')
+st.image(image, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.title('Dynamic Pressure Profile figure')
+st.markdown('Jaiyesh Chahar')
+st.markdown("Github: https://github.com/jaiyesh")
 
+st.sidebar.title('Inputs')
 
-
-st.sidebar.title('Dynamic Pressure Profile App inputs')
-st.title('Jaiyesh Chahar')
 k = st.sidebar.slider('Pem(md)',min_value = 10,max_value = 200,value =100)
 
 mu = st.sidebar.slider('Viscosity', min_value = 10,max_value=50, value =15)
 
 q = st.sidebar.slider('Flowrate(STB/Day)', min_value = 100, max_value = 200, value =150)
 
-re = st.sidebar.number_input('Outer Radius of Reservoir (feet)')
+re = st.sidebar.number_input('Outer Radius of Reservoir (feet)',min_value = 100,max_value=10000, value =3000)
 
-rw =st.sidebar.number_input('Wellbore Radius of Reservoir (feet)')
+rw =st.sidebar.number_input('Wellbore Radius of Reservoir (feet)',min_value =1,max_value=10, value =1)
 
-pe = st.sidebar.number_input('Pressure at the boundary of Reservoir(psi)')
+pe = st.sidebar.number_input('Pressure at the boundary of Reservoir(psi)',min_value = 100,max_value=10000, value =4000)
 
-B = st.sidebar.number_input('Formation Volume Factor(bbl/stb)')
+B = st.sidebar.number_input('Formation Volume Factor(bbl/stb)',min_value = 1,max_value=2, value =1)
 
-h= st.sidebar.number_input('Net pay thickness of Reservoir (feet)')
-# re = 3000
-# rw = 0.5
-# pe = 4000
-# B = 1
-# h = 30 #ft
+h= st.sidebar.number_input('Net pay thickness of Reservoir (feet)',min_value = 2,max_value=500, value =30)
 
 
-st.title('Dynamic Pressure Profile figure')
 
 r = np.linspace(rw,re,500)
 P = pe - (141.2*q*mu*B*(np.log(re/r))/k/h)
